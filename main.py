b42b83e76ccda67a7a897716edf3585c8c6b5973
Fitz <jaymarnitram08@gmail.com>
2026-08-16 08:54:53 +0800

    done

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
+
     book_name = sys.argv[1]
     book_texts = get_book_texts(book_name)
     word_count = get_num_words(book_texts)
